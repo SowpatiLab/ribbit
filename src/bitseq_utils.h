ecd06f87c5af3b52c24342197dd0992f8450ee40
Akshay Avvaru <avvaruakshay@gmail.com>
2025-03-07 12:36:18 -0700

    eof line added

--- src/bitseq_utils.h
+++ src/bitseq_utils.h
@@ -11,7 +11,9 @@ using namespace boost::multiprecision;
 uint32_t calculateRepeatClass(boost::dynamic_bitset<> &window, int &motif_length);
 
 int calculateAtomicity(uint256_t &motif, int &motif_length);
+
 int calculateAtomicity(uint32_t &motif, int &motif_length);
+
 int calculateAtomicityLongMotif(uint256_t &motif, int &motif_length);
 
 string calculateMotif(uint256_t motif_unit, int motif_length);
