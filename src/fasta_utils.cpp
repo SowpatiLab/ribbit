44eed4567b5914ab67fdde7a8fd8c26c79b2961f
Akshay Avvaru <avvaruakshay@gmail.com>
2025-04-23 13:12:58 -0600

    removed comments

--- src/fasta_utils.cpp
+++ src/fasta_utils.cpp
@@ -234,11 +234,6 @@ void processSequence(string sequence_id, string sequence, int window_length, int
         seed_positions_substut = processShiftXORswithSubstitutions(lshift_xor_bsets, N_bset, window_length, window_bitcount_threshold, seed_positions_perfect);
         failed_seeds = failedSeeds(seed_positions_perfect); failed_seeds += failedSeeds(seed_positions_substut);
 
-        // for (int _=0; _<seed_positions_perfect.size(); _++) {
-        //     if (get<0> (seed_positions_perfect[_]) >= 557310 && get<1> (seed_positions_perfect[_]) <= 559000 && motif_length==7)
-        //     cout << get<0> (seed_positions_perfect[_]) << "\t" << get<1> (seed_positions_perfect[_]) << "\t" << get<2> (seed_positions_perfect[_]) << "\t" << get<3> (seed_positions_perfect[_]) << "\n";
-        // }
-
         // generating the anchor bitsets for all shift sizes
         vector<boost::dynamic_bitset<>> lsxor_anchor_bsets;     // vector of dynamic bitsets for anchor bitsets
         generateAnchoredShiftXORs(lshift_xor_bsets, N_bset, lsxor_anchor_bsets, anchor_length);
@@ -319,9 +314,6 @@ void processSequence(string sequence_id, string sequence, int window_length, int
         if (seed_bset_size >= SEEDLEN_CUTOFF[seed_mlen - MINIMUM_MLEN]) {
             // process seed if it is alteast the size of the motif length
             processed_seeds += 1;
-            // if (seed_start >= 557310 && seed_end <= 558000 && seed_mlen==7) {
-            //     cout << "Seed: " << sequence_id << "\t" << seed_start << "\t" << seed_end << "\t" << seed_mlen << "\t" << seed_end-seed_start << "\t" << seed_type << "\n";
-            // }
             int slice_length = 20000 - 2*seed_mlen;
             if (seed_bset_size > slice_length) {
                 int slice_start = 0, slice_end = 0;
@@ -330,15 +322,16 @@ void processSequence(string sequence_id, string sequence, int window_length, int
                     else { slice_end = slice_start + slice_length; }
 
                     if (seed_mlen <= SMALL_MLEN_LIMIT) {
-                        processSeedMotifWise(tuple<int, int> { seed_start + slice_start, seed_start + slice_end }, 0, seed_mlen, seed_type, sequence_id, sequence,
-                                            sequence_length, lshift_xor_bsets[seed_mlen-MINIMUM_SHIFT], left_bset, right_bset, N_bset,
-                                            continuous_ones_threshold, out, aligner, filter, alignment, repeat_loci);
+                        processSeedMotifWise(tuple<int, int> { seed_start + slice_start, seed_start + slice_end }, 0, seed_mlen,
+                                             seed_type, sequence_id, sequence, sequence_length, lshift_xor_bsets[seed_mlen-MINIMUM_SHIFT],
+                                             left_bset, right_bset, N_bset, continuous_ones_threshold, out, aligner, filter, alignment, repeat_loci);
                     }
 
                     else {
-                        processSeed(tuple<int, int> { seed_start + slice_start, seed_start + slice_end }, 0, seed_mlen, seed_type, sequence_id, sequence, sequence_length,
-                                    lshift_xor_bsets[seed_mlen-MINIMUM_SHIFT], left_bset, right_bset, N_bset, continuous_ones_threshold,
-                                    out, lshift_xor_bsets, MATRIX, aligner, filter, alignment, repeat_loci);
+                        processSeed(tuple<int, int> { seed_start + slice_start, seed_start + slice_end }, 0, seed_mlen,
+                                    seed_type, sequence_id, sequence, sequence_length, lshift_xor_bsets[seed_mlen-MINIMUM_SHIFT],
+                                    left_bset, right_bset, N_bset, continuous_ones_threshold, out, lshift_xor_bsets, MATRIX,
+                                    aligner, filter, alignment, repeat_loci);
                     }
 
                     slice_start += slice_length - 500;
